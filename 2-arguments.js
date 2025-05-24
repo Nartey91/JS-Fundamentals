f6a94a0964c5ca3db5b831daed1e013bac2d8659
Prince Nartey <princekhay91@gmail.com>
2025-05-24 20:55:07 +0000

    Add script that checks number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+if (firstArg === undefined) {
+  console.log("No argument");
+} else if (secondArg === undefined) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
